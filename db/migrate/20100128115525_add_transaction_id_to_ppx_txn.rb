3f989b090688f7302dbf06960359092e00525f03
Trung Lê <joneslee85@gmail.com>
2012-12-02 23:24:18 +1100

    Respository has been moved to new home - remove all data to avoid confusion

--- db/migrate/20100128115525_add_transaction_id_to_ppx_txn.rb
+++
@@ -1,9 +0,0 @@
-class AddTransactionIdToPpxTxn < ActiveRecord::Migration
-  def self.up
-    add_column :paypal_txns, :transaction_id, :string
-  end
-
-  def self.down
-    remove_column :paypal_txns, :transaction_id
-  end
-end
--- lib/spree/checkouts_controller_with_paypal_express.rb
+++
@@ -1,7 +0,0 @@
-module Spree::CheckoutsControllerWithPaypalExpress
-  def self.included(target)
-    target.before_filter :redirect_to_paypal_express_form, :only => [:update]
-  end
-  
-  private
-end
